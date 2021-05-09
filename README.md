24f0ecb253b3df358b4073fed36dacf27a2e02a9
Francileide Barreto <barretofrancileide@gmail.com>
2021-05-09 19:30:03 -0300

    First client side commit

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Para executar os seguintes comandos:
 cd server
 make
 ./server
+
+Para que a pagina possa ser acessada de qualquer lugar é importante instalar algum provedor como DuckDNS ou NO-IP.
+
+Também é importante a criação de uma pasta com acesso pelo www-data para que o mesmo possa salvar as fotos
---
+++ client/login-page.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://192.168.0.178:12913/login?uname='+username+'&pass='+password);
+    xhr.onload = () =>{
+        // const data = JSON.parse(xhr.response);
+        console.log(data);
+    };
+    xhr.send();
+
+    // if (username === "user" && password === "web_dev") {
+    //     alert("You have successfully logged in.");
+    //     location.reload();
+    // } else {
+    //     loginErrorMsg.style.opacity = 1;
+    // }
+})
+
