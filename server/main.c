f2e3952ec253ecd92ddf711cf71ddfc4dd4e3f45
Francileide Barreto <barretofrancileide@gmail.com>
2021-05-09 22:33:18 -0300

    Server side opening and taking the photo done

--- server/main.c
+++ server/main.c
@@ -1,7 +1,42 @@
 #include "httpd.h"
 #include <mysql.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+#include <signal.h>
+
+//---Macros---//
+
+#define GPIO4 	4
+#define HIGH    1
+#define LOW 	0
+#define INPUT   0
+#define OUTPUT  1
+
+//---Prototipos das funcoes---//
+
+bool access_gpio(int pin);
+bool export_gpio(int pin);
+bool direction_gpio(int pin, int direction);
+bool value_gpio(int pin, int value);
+bool unexport_gpio(int pin);
+void delay(float time);
+void finalization(int nsingnal);
 int getUser(char uname[255], char pass[255]);
+int abrir (char id[255]);
+
+
+//---Variaveis Globais--------//
+int arquive, pin=GPIO4;
+float timeSleep=0.5;
+char buffer[3];
+char path[35];
+//----------------------------//
 
 int main(int c, char** v)
 {
@@ -17,8 +52,8 @@ void route()
     {
         printf("HTTP/1.1 200 OK\r\n");
         printf("Access-Control-Allow-Origin: http://192.168.0.178\r\n\r\n");
-        char uname[255];
-        char pass[255];
+        char uname[255]={'\0'};
+        char pass[255]={'\0'};
         char *init = strchr(qs,'=');
         char *end = strchr(qs,'&');
         strncpy(uname,init+1,end-init-1);
@@ -27,6 +62,19 @@ void route()
         strncpy(pass,init+1,end-init);
         int i;
         i=getUser(uname,pass);
+    }
+
+    ROUTE_GET("/abrir")
+    {
+        printf("HTTP/1.1 200 OK\r\n");
+        printf("Access-Control-Allow-Origin: http://192.168.0.178\r\n\r\n");
+        char id[255];
+        char *init = strchr(qs,'=');
+        char *end = &qs[strlen(qs)-1];
+        strncpy(id,init+1,end-init);
+        int i;
+        i=abrir(id);
+        i=abrir(id);
     }
 
     ROUTE_POST("/")
@@ -100,4 +148,183 @@ int getUser(char uname[255], char pass[255])
  mysql_close(conn);
 
  return 1;
+}
+
+int abrir (char id[255])
+{
+	signal(SIGINT, finalization);
+	// === Verifica se o pino ja foi exportado === //
+	if(access_gpio(pin))
+  {
+		// === Export do Pino === //
+		if (export_gpio(pin))
+		{
+		// === Configurando Direcao do Pino === //
+			if(direction_gpio(pin, OUTPUT))
+			{
+				system("fswebcam /home/pi/Documents/porteiroremoto/www-data/image2.jpg");
+					sleep(1);
+			}
+			else
+			{
+				printf("Ocorreu um problema na configuracao do pino como I/O\n");
+				return 2;
+			}
+			 // === Desvinculando o pino == //
+			if(unexport_gpio(pin))
+			{
+					return 0;
+			}
+			else
+			{
+					printf("Ocorreu um problema para finalizar a utilizacao do pino\n");
+					return 2;
+			}
+
+		}
+		else
+		{
+			printf("Ocorreu um problema no export do pino\n");
+                        return 1;
+		}
+	}
+	else
+	{
+		printf("Pino ja foi exportado! \n");
+		delay(0.5);
+		printf("Configurando as caracteristicas de utilizacao! \n");
+		delay(0.5);
+
+		if(direction_gpio(pin, OUTPUT))
+		{
+			system("fswebcam /home/pi/Documents/porteiroremoto/www-data/image2.jpg");
+			printf("GPIO%d configurada como OUTPUT! \n", pin);
+			delay(0.5);
+			printf("Iniciando o processo de acionamento\n");
+			delay(0.5);
+			if(unexport_gpio(pin))
+			{
+				return 0;
+			}
+			else
+			{
+				printf("Ocorreu um problema para finalizar a utilizacao do pino\n");
+				return 2;
+			}
+	  	}
+		else
+		{
+				printf("Ocorreu um problema na configuracao do pino como I/O\n");
+				return 2;
+		}
+	}
+}
+
+void finalization(int nsignal)
+{
+	// === Desvinculando o pino == //
+
+        if(unexport_gpio(pin))
+        {
+		delay(0.5);
+                printf("\nAplicando um unexport no pino\n");
+		delay(0.5);
+		printf("Programa finalizado...\n");
+		exit(0);
+        }
+}
+
+bool access_gpio(int pin)
+{
+        snprintf(path, 35, "/sys/class/gpio/gpio%d/direction", pin);
+        if (access(path, 0) == -1) // Arquivo nao existe
+        {
+                return true;
+        }
+        else // Arquivo existe
+        {
+                return false;
+        }
+}
+
+bool export_gpio(int pin)
+{
+        arquive = open ("/sys/class/gpio/export", O_WRONLY);
+        if (arquive==-1)
+        {
+                printf("Arquivo abriu incorretamente\n");
+                return false;
+        }
+        snprintf(buffer, 3, "%d", pin);
+        if(write(arquive, buffer, 3) == -1)
+        {
+                close(arquive);
+                return false;
+        }
+
+        close(arquive);
+
+	return true;
+}
+
+bool direction_gpio(int pin, int direction)
+{
+	arquive=0;
+        snprintf(path, 35, "/sys/class/gpio/gpio%d/direction", pin);
+        arquive = open (path, O_WRONLY);
+        if (arquive==-1)
+        	return false;
+
+        snprintf(buffer, 3, "%d", pin);
+        if (write( arquive, ((direction == INPUT)?"in":"out"), 3 )==-1)
+        {
+                close(arquive);
+                return false;
+        }
+        close(arquive);
+	return true;
+}
+
+bool value_gpio(int pin, int value)
+{
+	arquive=0;
+        snprintf(path, 35, "/sys/class/gpio/gpio%d/value", pin);
+        arquive = open(path, O_WRONLY);
+        if (arquive == -1)
+        {
+                return false;
+        }
+        if (write (arquive, ((value == HIGH)?"1":"0"), 1) == -1)
+        {
+                close(arquive);
+                return false;
+        }
+	close(arquive);
+	return true;
+}
+
+bool unexport_gpio(int pin)
+{
+	arquive = open ("/sys/class/gpio/unexport", O_WRONLY);
+        if (arquive==-1)
+        {
+                printf("Arquivo abriu incorretamente\n");
+                return false;
+        }
+        if(write(arquive, buffer, 3) == -1)
+        {
+                close(arquive);
+                return false;
+        }
+        return true;
+}
+
+void delay(float time)
+{
+	struct timespec t;
+	int seg;
+	seg = time;
+	t.tv_sec = seg;
+	t.tv_nsec = (time-seg)*1e9;
+	nanosleep(&t, NULL);
 }
